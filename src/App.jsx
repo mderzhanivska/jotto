0c65c4d0f38d589d6a001a2a09c253f0364c2a2c
mderzhanivska <mderzhanivska@talix.com>
2022-05-24 15:03:53 +0300

    add: first test for button

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [buttonColor, setButtonColor] = useState('red');
+  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+
+  return (
+    <div>
+      <button
+        onClick={() => setButtonColor(newButtonColor)}
+        style={{ backgroundColor: buttonColor }}
+      >
+        Change to {newButtonColor}
+      </button>
+    </div>
+  );
+}
+
+export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('button has correct initial color', () => {
+    render( < App / > );
+    const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+    expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+    fireEvent.click(colorButton);
+    expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+    expect(colorButton.textContent).toBe('Change to red');
 });
